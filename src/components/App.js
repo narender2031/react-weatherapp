a1b420ef900485bccc1e1b57afd2e0b4fc3f831e
narender <narender2031@gmail.com>
2019-02-12 18:20:17 +0530

    update

---
+++ src/components/App.js
@@ -0,0 +1,77 @@
+import React from 'react'
+
+
+// app compnnents
+import Title from './Title'
+import Form from './Form'
+import Weather  from './Weather'
+
+
+const api_key = "f6d0e0eff38aa438783610f983d9bb73"
+class App extends React.Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.city.value;
+    const country = e.target.country.value;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${api_key}`);
+    const data = await api_call.json();
+    if (city && country){
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+      })
+    }else{
+      this.setState({
+        temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: "Please! enter the value"
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <div className="wrapper">
+          <div className="main">
+            <div className="container">
+              <div className="row">
+                <div className="col-xs-5 title-container">
+                  <Title />
+                </div>
+                <div className="col-xs-7 form-container">
+                  <Form getWeather = {this.getWeather}/>
+                  <Weather 
+                    temperature = {this.state.temperature}
+                    city = {this.state.city}
+                    country = {this.state.country}
+                    humidity = {this.state.humidity}
+                    description = {this.state.description}
+                    error = {this.state.error}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/components/Title.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Title = () => {
+    return(
+        <div>
+            <h1 className="title-container__title">Weather</h1>
+            <p className="title-container__subtitle">Find out tempeature and more</p>
+        </div>
+    )
+}
+
+
+export default Title
